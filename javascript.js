43049952f97342c6366581c19df2ffacb6d299c9
Roman <romanrodriguez@users.noreply.github.com>
2017-06-16 21:05:26 -0500

    Add tweet quote

--- javascript.js
+++ javascript.js
@@ -1,40 +1,33 @@
-"use strict";
+var tweetUrl = "";
 var quotes = [
-    'Programs must be written for people to read, and only incidentally for machines to execute. - <em>Harold Abelson</em>',
-    'So much technology, so little talent. - <em>Vernor Vinge</em>',
-    'The greatest achievement of humanity is not its works of art, science, or technology, but the recognition of its own dysfunction. - <em>Eckhart Tolle</em>',
-    'TV and the Internet are good because they keep stupid people from spending too much time out in public. - <em>Douglas Coupland</em>',
-    'Computers are useless. They can only give you answers. - <em>Pablo Picasso</em>',
-    'Just because something doesn’t do what you planned it to do doesn’t mean it’s useless. - <em>Thomas Edison</em>',
-    'One machine can do the work of fifty ordinary men. No machine can do the work of one extraordinary man. - <em>Elbert Hubbard</em>',
-    'Humanity is acquiring all the right technology for all the wrong reasons. - <em>R. Buckminster Fuller</em>',
-    'Technological progress has merely provided us with more efficient means for going backwards. - <em>Aldous Huxley</em>',
-    'The human spirit must prevail over technology. - <em>Albert Einstein</em>'
+    'Programs must be written for people to read, and only incidentally for machines to execute. - Harold Abelson',
+    'So much technology, so little talent. - Vernor Vinge',
+    'The greatest achievement of humanity is not its works of art, science, or technology, but the recognition of its own dysfunction. - Eckhart Tolle',
+    'TV and the Internet are good because they keep stupid people from spending too much time out in public. - Douglas Coupland',
+    'Computers are useless. They can only give you answers. - Pablo Picasso',
+    'Just because something doesn’t do what you planned it to do doesn’t mean it’s useless. - Thomas Edison',
+    'One machine can do the work of fifty ordinary men. No machine can do the work of one extraordinary man. - Elbert Hubbard',
+    'Humanity is acquiring all the right technology for all the wrong reasons. - R. Buckminster Fuller',
+    'Technological progress has merely provided us with more efficient means for going backwards. - Aldous Huxley',
+    'The human spirit must prevail over technology. - Albert Einstein'
 ];
-// Call Math.floor() function, which takes a parameter
-// and rounds the number downward to the nearest integer.
-// Pass in Math.random() as a parameter into Math.floor().
-// The Math.random() function will generate a random decimal
-// number between 0 and 1. We multiply it by the number of
-// quotes.length array to generate a random number.
+
 function newQuote() {
+	// Call Math.floor() function, which takes a parameter
+	// and rounds the number downward to the nearest integer.
+	// Pass in Math.random() as a parameter into Math.floor().
+	// The Math.random() function will generate a random decimal
+	// number between 0 and 1. We multiply it by the number of
+	// quotes.length array to generate a random number.
     var randomNumber = Math.floor(Math.random() * (quotes.length));
+    var randomQuote = quotes[randomNumber];
     // To randomly retrieve a quote from the quotes array and place
     // the quote into our HTML. Use the .innerHTML method to pass
     // a randomly retrieved quote as the value.
-    document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
+    document.getElementById('quoteDisplay').innerHTML = randomQuote;
+    var shareQuote = randomQuote;
+    // Use regex to replace spaces in a string with + to work with the URL.
+    shareQuote.replace(/\s/g, "+");
+    tweetUrl = "https://twitter.com/intent/tweet?text=" + randomQuote;
+    document.getElementById('tweet').setAttribute("href", tweetUrl);
 }
-
-// Twitter button event handler
-var quoteContainer = document.getElementById('quoteDisplay');
-var textContent = quoteContainer.textContent;
-
-$('#tweet').click(function() {
-    window.open('https://twitter.com/intent/tweet?text=' + textContent);
-});
-
-
-// $('#tweet').click(function(){
-// 	var tweetQuote = $('#quoteDisplay').innerHTML;
-// 	window.open('https://twitter.com/intent/tweet?text=' + tweetQuote);
-// });
